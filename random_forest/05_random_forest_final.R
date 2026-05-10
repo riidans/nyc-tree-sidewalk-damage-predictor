51899cacc59eb9a33296e09e258643d1555fb7a5
riidans <109998029+riidans@users.noreply.github.com>
2026-05-09 20:12:39 -0400

    update final random forest model script with f2 score

--- random_forest/05_random_forest_final.R
+++ random_forest/05_random_forest_final.R
@@ -1,6 +1,6 @@
 library(rstudioapi)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-tree_data = readRDS("../cleaned_tree_data.rds")
+tree_data = readRDS("../MTH4330/cleaned_tree_data.rds")
 
 library(ranger)
 tree_data$sidewalk = as.factor(tree_data$sidewalk)
@@ -31,7 +31,7 @@ rf_model = ranger(
             borough + curb_loc + nta_name + steward +
             latitude + longitude,
   data = train_data,
-  mtry = 16, # Taken from 04_random_forest_tuning
+  mtry = 12, # Taken from 04_random_forest_tuning
   min.node.size = 15, # Taken from 04_random_forest_tuning
   num.trees = 500,
   probability = TRUE,
@@ -40,12 +40,37 @@ rf_model = ranger(
   # change this to permutation for the feature importance graph, otherwise keep as none
 )
 
-optimal_threshold = 0.45 # Taken from 03_random_forest_weights
+dev_prob = predict(rf_model, data = dev_data)$predictions[, "1"]
+
+metrics_table = data.frame()
+
+for (threshold in seq(0.1, 0.9, by = 0.01)) {
+  dev_pred = ifelse(dev_prob > threshold, 1, 0)
+
+  tp = sum(dev_data$sidewalk == 1 & dev_pred == 1)
+  tn = sum(dev_data$sidewalk == 0 & dev_pred == 0)
+  fp = sum(dev_data$sidewalk == 0 & dev_pred == 1)
+  fn = sum(dev_data$sidewalk == 1 & dev_pred == 0)
+
+  accuracy  = (tp + tn) / (tp + tn + fp + fn)
+  precision = tp / (tp + fp)
+  recall    = tp / (tp + fn)
+  f1  = 2 * (precision * recall) / (precision + recall)
+  f2 = 5 * (precision * recall) / ((4 * precision) + recall)
+
+  current_row = data.frame(threshold, accuracy, precision, recall, f1, f2)
+  metrics_table = rbind(metrics_table, current_row)
+}
+
+# Takes the threshold with the highest F1 score metric
+
+optimal_threshold_f1 = metrics_table[which.max(metrics_table$f1), ]$threshold #0.46
+optimal_threshold_f2 = metrics_table[which.max(metrics_table$f2), ]$threshold #0.23
 
 # RANDOM FOREST MODEL METRICS ON TEST SET
 
 test_prob = predict(rf_model, data = test_data)$predictions[, "1"]
-test_pred = ifelse(test_prob > optimal_threshold, 1, 0)
+test_pred = ifelse(test_prob > optimal_threshold_f1, 1, 0)
 
 tp = sum(test_data$sidewalk == 1 & test_pred == 1)
 tn = sum(test_data$sidewalk == 0 & test_pred == 0)
@@ -56,9 +81,33 @@ accuracy  = (tp + tn) / (tp + tn + fp + fn)
 precision = tp / (tp + fp)
 recall    = tp / (tp + fn)
 f1  = 2 * (precision * recall) / (precision + recall)
+f2 = 5 * (precision * recall) / ((4 * precision) + recall)
 
-cat("Random Forest + Optimized Threshold (", optimal_threshold, ") + Class Weights + Tuned Hyperparameters Metrics \n", 
+cat("Random Forest + Optimized Threshold for F1 (", optimal_threshold_f1, ")\n", 
 "Accuracy: ", round(accuracy, 4), "\n", 
 "Precision:", round(precision, 4), "\n", 
 "Recall:   ", round(recall, 4), "\n", 
-"F1 Score: ", round(f1, 4), "\n")  
+"F1 Score: ", round(f1, 4), "\n",
+"F2 Score: ", round(f2, 4), "\n")  
+
+# ==============================================================
+
+test_pred = ifelse(test_prob > optimal_threshold_f2, 1, 0)
+
+tp = sum(test_data$sidewalk == 1 & test_pred == 1)
+tn = sum(test_data$sidewalk == 0 & test_pred == 0)
+fp = sum(test_data$sidewalk == 0 & test_pred == 1)
+fn = sum(test_data$sidewalk == 1 & test_pred == 0)
+
+accuracy  = (tp + tn) / (tp + tn + fp + fn)
+precision = tp / (tp + fp)
+recall    = tp / (tp + fn)
+f1  = 2 * (precision * recall) / (precision + recall)
+f2 = 5 * (precision * recall) / ((4 * precision) + recall)
+ 
+cat("Random Forest + Optimized Threshold for F2 (", optimal_threshold_f2, ")\n", 
+"Accuracy: ", round(accuracy, 4), "\n", 
+"Precision:", round(precision, 4), "\n", 
+"Recall:   ", round(recall, 4), "\n", 
+"F1 Score: ", round(f1, 4), "\n",
+"F2 Score: ", round(f2, 4), "\n")  
